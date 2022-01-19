5a268b2a7b9c52b60e726c7c90d2023d6c9c4a09
bradlindblad <bradley.lindblad@gmail.com>
2022-01-18 21:00:23 -0600

    ...

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 from bs4 import BeautifulSoup as bs
 import requests
-import telegram_send
+from telethon import TelegramClient
 
-chat_id = "5090479049"
+chat_id = 509047904
 api_key = "5090479049:AAFKlZFwLsIkttkgMvTgtU0OGTFEwChXPD4"
 
 def getSpecials():
@@ -22,39 +22,20 @@ def getSpecials():
 
     return answer
 
+# def send_telegram_message():
+    
+#     client = TelegramClient('session_name', chat_id, api_key)
+#        #Start the process
+#     client.start()
+#        #Send the message
+#     client.send_message(chat_id = chat_id, msg = "wahtupp")
 
-# def send_telegram_message(message: str,
-#                           chat_id: str,
-#                           api_key: str,
-#                           proxy_username: str = None,
-#                           proxy_password: str = None,
-# 		  proxy_url: str = None):
-#     responses = {}
-
-#     proxies = None
-#     if proxy_url is not None:
-#         proxies = {
-#             'https': f'http://{username}:{password}@{proxy_url}',
-#             'http': f'http://{username}:{password}@{proxy_url}'
-#         }
-#         headers = {'Content-Type': 'application/json',
-#                    'Proxy-Authorization': 'Basic base64'}
-#         data_dict = {'chat_id': chat_id,
-#                      'text': message,
-#                      'parse_mode': 'HTML',
-#                      'disable_notification': True}
-#         data = json.dumps(data_dict)
-#         url = f'https://api.telegram.org/bot{api_key}/sendMessage'
-#         response = requests.post(url,
-#                                  data=data,
-#                                  headers=headers,
-#                                  proxies=proxies,
-#                                  verify=False)
-#         return response
 
 if __name__ == "__main__":
     
     # Get the specials
     meats = getSpecials()
     
-    telegram_send.send(messages=[meats],conf = "getmeat.conf")
+    # telegram_send.send(messages=[meats],conf = "getmeat.conf")
+    # send_telegram_message(meats)
+    
