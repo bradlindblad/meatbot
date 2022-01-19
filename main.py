44d470cad64c9a1b1c43099fac8ee4135768a133
bradlindblad <bradley.lindblad@gmail.com>
2022-01-18 21:43:11 -0600

    pray

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
 from bs4 import BeautifulSoup as bs
 import requests
-from telethon import TelegramClient
+import telepot
 
 chat_id = 509047904
 api_key = "5090479049:AAFKlZFwLsIkttkgMvTgtU0OGTFEwChXPD4"
 
-def getSpecials():
+def getSpecials(msg):
     URL = "http://www.meatsbyjohnandwayne.com/weeklyspecials.html"
 
     response = requests.get(URL)
@@ -22,13 +22,11 @@ def getSpecials():
 
     return answer
 
-# def send_telegram_message():
+def send_telegram_message():
     
-#     client = TelegramClient('session_name', chat_id, api_key)
-#        #Start the process
-#     client.start()
-#        #Send the message
-#     client.send_message(chat_id = chat_id, msg = "wahtupp")
+    bot = telepot.Bot(api_key)
+    bot.getMe()
+    bot.sendMessage(1690960383, msg)
 
 
 if __name__ == "__main__":
@@ -36,7 +34,6 @@ if __name__ == "__main__":
     # Get the specials
     meats = getSpecials()
     
-    # telegram_send.send(messages=[meats],conf = "getmeat.conf")
-    # send_telegram_message(meats)
-    print(meats)
+    send_telegram_message(meats)
+    
     
